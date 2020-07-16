38da24e0f4537f9d728616902db3b2625f0d7b14
Andrii Kochmaruk <luckyanry@gmail.com>
2020-07-16 21:49:09 +0200

    created contact form for users with name and phone number

--- src/App.js
+++ src/App.js
@@ -1,26 +1,105 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { v4 as uuidv4 } from "uuid";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// <li>Rosie Simpson</li>
+// <li>Hermion Kline</li>
+// <li>Eden Clements</li>
+
+class App extends Component {
+  formInitialState = {
+    name: "",
+    number: "",
+  };
+
+  state = {
+    contacts: [],
+    ...this.formInitialState,
+  };
+
+  inputHandler = ({ target }) => {
+    const { value, name } = target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  submitHandler = (e) => {
+    const { name, number } = this.state;
+    e.preventDefault();
+
+    const singleContact = {
+      name,
+      number,
+      id: uuidv4(),
+    };
+
+    this.addContact(singleContact);
+    this.reset();
+  };
+
+  reset = () => {
+    this.setState({ ...this.formInitialState });
+  };
+
+  addContact = (contactObj) => {
+    this.setState((prev) => ({
+      contacts: [...prev.contacts, contactObj],
+    }));
+  };
+
+  deleteContact = (id) => {
+    this.setState((prev) => ({
+      contacts: prev.contacts.filter((contact) => contact.id !== id),
+    }));
+  };
+
+  render() {
+    const { contacts, name, number } = this.state;
+    return (
+      <>
+        <h2>Phonebook</h2>
+        <form className="ContactsForm" onSubmit={this.submitHandler}>
+          <label className="InputName">
+            Name
+            <br />
+            <input
+              className="InputForm"
+              type="text"
+              name="name"
+              placeholder="Add name"
+              value={name}
+              onChange={this.inputHandler}
+            />
+          </label>
+          <br />
+          <label className="InputName">
+            Number
+            <br />
+            <input
+              className="InputForm"
+              type="text"
+              name="number"
+              placeholder="Add phone number"
+              value={number}
+              onChange={this.inputHandler}
+            />
+          </label>
+          <br />
+          <button type="submit">Add contact</button>
+        </form>
+
+        <h2>Contacts</h2>
+        <ul>
+          {contacts.map(({ name, id, number }) => (
+            <li key={id}>
+              {name}: {number}
+            </li>
+          ))}
+        </ul>
+      </>
+    );
+  }
 }
 
 export default App;
